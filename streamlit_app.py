f23aa62686a6c65d77e5f2f4dbe81a3ee19dace3
Min Khant Maung Maung <minkhantmgmg.mk19@gmail.com>
2026-09-16 22:15:46 +0700

    Comment out st.dataframe and st.stop
    Comment out dataframe display and stop commands.

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,8 @@ my_dataframe = session.table('SMOOTHIES.PUBLIC.FRUIT_OPTIONS').select(col('SEARC
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
